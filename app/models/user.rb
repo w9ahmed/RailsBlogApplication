d998bd27a09f3f60e5b2ae8dd7f48b97ad089017
Ahmed Sami <wenning9ahmed@hotmail.com>
2014-11-26 19:58:53 +0200

    Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+	validates_uniqueness_of :email
+	validates_length_of :email, :within => 5..50
+	validates_format_of :email, :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2-4}$/i
+
+	validates_length_of :password, :within => 6..16
+	validates_confirmation_of :password
+
 	has_one :profile
 	has_many :articles, -> { order('published_at DESC, title ASC') }, :dependent => :nullify
+	has_many :replies, :through => :articles, :source => :comments
 end
