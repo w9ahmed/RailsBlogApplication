503282ebb01aa4fdbea596e1e746b86c087ddb8f
Ahmed Sami <wenning9ahmed@hotmail.com>
2014-11-27 12:20:27 +0200

    Action Pack

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
-Rails.application.routes.draw do
+Blog::Application.routes.draw do
+  root :to => 'articles#index'
   resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
