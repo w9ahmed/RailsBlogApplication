cf4d8c10e53de457b2341f827490d20f5053c452
Ahmed Sami <wenning9ahmed@hotmail.com>
2014-12-01 14:00:32 +0200

    Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Blog::Application.routes.draw do
   root :to => 'articles#index'
   resources :articles
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
