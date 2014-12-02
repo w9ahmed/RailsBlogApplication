7a92b5257fb8121acee84e9f24017a06d6c8aff7
Ahmed Sami <wenning9ahmed@hotmail.com>
2014-12-02 14:01:21 +0200

    Session Controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Blog::Application.routes.draw do
     resources :comments
   end
   resources :users
+  resources :session
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
