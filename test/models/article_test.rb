40ce55e3b456e02757c915a7bb2ab21f81fa9a35
Ahmed Sami <wenning9ahmed@hotmail.com>
2014-12-11 12:43:18 +0200

    Fixtures Edit

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should create article" do
+  	article = Article.new
+  	article.user = users(:eugene)
+  	article.title = "Test Article"
+  	article.body = "Test Body"
+
+  	assert article.save
+  end
+
 end
